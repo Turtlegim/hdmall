0b81527c0a0dd5e5c62c770e2c5f0302b12e38cb
turtlegim <turtlegim@gmail.com>
2022-03-17 10:01:52 +0900

    03.17 10:00 pboarddao 수정.(프로시저 오버로딩)

--- src/main/java/com/hdmall/dao/PBoardDAO.java
+++ src/main/java/com/hdmall/dao/PBoardDAO.java
@@ -24,67 +24,69 @@ public class PBoardDAO {
 	private Connection conn = null;
 	private PreparedStatement pstmt = null;
 	private ResultSet rs = null;
-    private OracleCallableStatement ocstmt = null;
+	private OracleCallableStatement ocstmt = null;
 
-	private PBoardDAO() { } // 싱글턴 패턴 처리
+	private PBoardDAO() {
+	} // 싱글턴 패턴 처리
+
 	private static PBoardDAO instance = new PBoardDAO();
+
 	public static PBoardDAO getInstance() {
 		return instance;
 	}
-	
+
 	// Hit 상위 7개의 상품 목록을 가져오는 함수.
 	public ArrayList<ProductVO> listTophitsPBoard() {
-	    ArrayList<ProductVO> productList = new ArrayList<>();
-	    String sql = "select * from best_view";
-	    Connection conn = null;
-	    PreparedStatement pstmt = null;
-	    ResultSet rs = null;    
-	    try {
-	      conn = DBManager.getConnection();
-	      pstmt = conn.prepareStatement(sql);
-	      rs = pstmt.executeQuery();
-	      while (rs.next()) {
-	    	  ProductVO product = new ProductVO();
-	    	  product.setId(rs.getString("prod_id"));
-		      product.setName(rs.getString("pbtitle"));
-		      product.setCate_no(rs.getString("cate"));
-		      product.setContext(rs.getString("pbtext"));
-		      product.setImg(rs.getString("image"));
-		      productList.add(product);
-	      }
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	    } finally {
-	      DBManager.close(conn, pstmt, rs);
-	    }
-	    return productList;
-	  }
-	
+		ArrayList<ProductVO> productList = new ArrayList<>();
+		String sql = "{call BEST_PACK.listTophitsPBoard_PROC(?)}"; // 패키지 내부에서 프로시저 오버로딩(입력값 없음)
+		try {
+			conn = DBManager.getConnection();
+			cstmt = conn.prepareCall(sql);
+			cstmt.registerOutParameter(1, OracleTypes.CURSOR);
+			cstmt.executeQuery();
+			rs = (ResultSet) cstmt.getObject(1);
+			while (rs.next()) {
+				ProductVO product = new ProductVO();
+				product.setId(rs.getString("prod_id"));
+				product.setName(rs.getString("pbtitle"));
+				product.setCate_no(rs.getString("cate"));
+				product.setContext(rs.getString("pbtext"));
+				product.setImg(rs.getString("image"));
+				productList.add(product);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, cstmt, rs);
+		}
+		return productList;
+	}
+
 	// 카테고리별 Hit 상위 7개의 상품 목록을 가져오는 함수.
 	public ArrayList<ProductVO> listTophitsPBoard(String cateno) {
-	    ArrayList<ProductVO> productList = new ArrayList<>();
-	    String sql = "{call listTophitsPBoard_PROC(?, ?)}";
-	    try {
-	      conn = DBManager.getConnection();
-	      cstmt = conn.prepareCall(sql);
-	      cstmt.setString(1, cateno);
-	      cstmt.registerOutParameter(2, OracleTypes.CURSOR);
-	      cstmt.executeQuery();
-	      rs = (ResultSet) cstmt.getObject(2);
-	      while (rs.next()) {
-	    	  ProductVO product = new ProductVO();
-	    	  product.setId(rs.getString("prod_id"));
-		      product.setName(rs.getString("pbtitle"));
-		      product.setCate_no(rs.getString("cate"));
-		      product.setContext(rs.getString("pbtext"));
-		      product.setImg(rs.getString("image"));
-		      productList.add(product);
-	      }
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	    } finally {
-	      DBManager.close(conn, cstmt, rs);
-	    }
-	    return productList;
+		ArrayList<ProductVO> productList = new ArrayList<>();
+		String sql = "{call BEST_PACK.listTophitsPBoard_PROC(?, ?)}"; // 패키지 내부에서 프로시저 오버로딩(입력 하나)
+		try {
+			conn = DBManager.getConnection();
+			cstmt = conn.prepareCall(sql);
+			cstmt.setString(1, cateno);
+			cstmt.registerOutParameter(2, OracleTypes.CURSOR);
+			cstmt.executeQuery();
+			rs = (ResultSet) cstmt.getObject(2);
+			while (rs.next()) {
+				ProductVO product = new ProductVO();
+				product.setId(rs.getString("prod_id"));
+				product.setName(rs.getString("pbtitle"));
+				product.setCate_no(rs.getString("cate"));
+				product.setContext(rs.getString("pbtext"));
+				product.setImg(rs.getString("image"));
+				productList.add(product);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, cstmt, rs);
+		}
+		return productList;
 	}
 }
